6595a4e6f3b656db034f684392d312784bde1323
Thong Nguyen <thongnb94@gmail.com>
2019-07-24 12:21:04 +0700

    DAO

--- src/com/msita/jspservlet/dao/BookDAO.java
+++ src/com/msita/jspservlet/dao/BookDAO.java
@@ -3,10 +3,228 @@
  */
 package com.msita.jspservlet.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import com.msita.jspservlet.bo.Book;
+import com.msita.jspservlet.dbconnection.ConnectionFactory;
+
 /**
  * @author thongnguyen
  *
  */
 public class BookDAO {
+	public ArrayList<Book> getAllUser(){
+		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
+		ArrayList<Book> bookList = new ArrayList<Book>();
+		
+		try {
+			connection = ConnectionFactory.getConnection();
+			String sql = "SELECT * FROM book";
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(sql);
+			while (resultSet.next()) {
+				bookList.add(convertToBook(resultSet));
+			}
+		} catch (SQLException e) {
+			//Handle errors for JDBC
+			e.printStackTrace();
+		} finally {
+			//finally block used to close resources
+	        if (resultSet != null) {
+                try {
+					resultSet.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
 
+            if (statement != null) {
+            	try {
+            		statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+
+            if (connection != null) {
+            	try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+		}
+		
+		return bookList;
+	}
+	
+	public ArrayList<Book> findBookByName(String name){
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		ArrayList<Book> bookList = new ArrayList<Book>();
+		
+		try {
+			connection = ConnectionFactory.getConnection();
+			String sql = "SELECT * FROM book WHERE name = ?";
+			preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, name);
+			resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				bookList.add(convertToBook(resultSet));
+			}
+		} catch (SQLException e) {
+			//Handle errors for JDBC
+			e.printStackTrace();
+		} finally {
+			//finally block used to close resources
+	        if (resultSet != null) {
+                try {
+					resultSet.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+
+            if (preparedStatement != null) {
+            	try {
+            		preparedStatement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+
+            if (connection != null) {
+            	try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+		}
+		
+		return bookList;
+	}
+	
+	public void insertBook(Book book){
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		String insertSQL = "INSERT INTO book (name,author,publisher,price) VALUES (?,?,?,?)";
+		try {
+			connection = ConnectionFactory.getConnection();	
+			preparedStatement = connection.prepareStatement(insertSQL);
+			preparedStatement.setString(1, book.getName());
+			preparedStatement.setString(2, book.getAuthor());
+			preparedStatement.setString(3, book.getPublisher());
+			preparedStatement.setInt(4, book.getPrice());
+			preparedStatement.executeUpdate();
+	        
+		} catch (SQLException e) {
+			//Handle errors for JDBC
+			e.printStackTrace();
+		} finally {
+			//finally block used to close resources
+            if (preparedStatement != null) {
+            	try {
+					preparedStatement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+
+            if (connection != null) {
+            	try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+		}
+	}
+	
+	public void updateBook(Book book) {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			
+			String updateSql = "UPDATE book SET name = ?, author = ?, publisher = ?, price = ? WHERE id = ?";
+			connection = ConnectionFactory.getConnection();
+			preparedStatement = connection.prepareStatement(updateSql);
+			preparedStatement.setString(1, book.getName());
+			preparedStatement.setString(2, book.getAuthor());
+			preparedStatement.setString(3, book.getPublisher());
+			preparedStatement.setInt(4, book.getPrice());
+			preparedStatement.setInt(5, book.getId());
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			//Handle errors for JDBC
+			e.printStackTrace();
+		} finally {
+			//finally block used to close resources
+			if (preparedStatement != null) {
+            	try {
+            		preparedStatement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+
+
+            if (connection != null) {
+            	try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+		}
+	}
+	
+	public void deleteUserList(int bookId) {
+		Connection connection = null;
+		Statement statement = null;
+		try {
+			String sql = "DELETE FROM user WHERE id =" + bookId;
+			connection = ConnectionFactory.getConnection();
+			statement = connection.createStatement();
+			statement.executeUpdate(sql);
+		} catch (SQLException e) {
+			//Handle errors for JDBC
+			e.printStackTrace();
+		} finally {
+			//finally block used to close resources
+            if (statement != null) {
+            	try {
+            		statement.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+
+            if (connection != null) {
+            	try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+            }
+		}
+	}
+	
+	private static Book convertToBook(ResultSet rs) throws SQLException {
+		Book book = new Book();
+		book.setId(rs.getInt(1));		
+		book.setName(rs.getString(2));		
+		book.setAuthor(rs.getString(3));		
+		book.setPublisher(rs.getString(4));	
+		book.setPrice(rs.getInt(4));	
+		return book;
+	}
 }
